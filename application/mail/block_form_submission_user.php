3e2747dda187ca14f20cb48631eaf143d48c6e43
Katz Ueno <katz@concrete5.co.jp>
2021-02-19 21:19:07 +0900

    Add email template customization option

--- application/mail/block_form_submission_user.php
+++ application/mail/block_form_submission_user.php
@@ -5,6 +5,9 @@ foreach($questionAnswerPairs as $questionAnswerPair){
 	$submittedData .= $questionAnswerPair['question']."\r\n".$questionAnswerPair['answerDisplay']."\r\n"."\r\n";
 } 
 $formDisplayUrl=URL::to($url_path) . '?qsid='.$questionSetId;
+
+//$subject =""; // 件名をカスタマイズする場合は、ここのコメントアウトを外して入れることで件名を設定できます。
+
 //echo $formDisplayUrl;
 $body = t("
 You have submitted the following message from the form \"%s\" through our website.
